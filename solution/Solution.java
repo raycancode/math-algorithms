b2a1e73ee3d394bd0c66e381a668baaa911ff6f5
ray <raytran.golden@gmail.com>
2026-04-15 16:23:26 +0700

    Increasing Triplet Subsequence

--- solution/Solution.java
+++ solution/Solution.java
@@ -150,5 +150,17 @@ public class Solution {
         return res;
     }
 
+    public boolean increasingTriplet(int[] nums) {
+        int first = Integer.MAX_VALUE, second = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]<=first) first=nums[i];
+            else if(nums[i]<=second) second = nums[i];
+            else return true;
+        }
+        
+        return false;
+        
+    }
+
     
 }
