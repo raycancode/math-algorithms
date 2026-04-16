ddcaf836d5b58edd1c0f9cbcfd33241ad89872ef
ray <raytran.golden@gmail.com>
2026-04-16 11:16:15 +0700

    135. Candy

--- solution/Solution.java
+++ solution/Solution.java
@@ -178,5 +178,27 @@ public class Solution {
         return jump;
     }
 
+    public int candy(int[] ratings) {
+        int n = ratings.length;
+        int[] cans = new int[n];
+        
+        Arrays.fill(cans, 1);
+
+        for (int i=1;i<n;i++){
+            if (ratings[i]>ratings[i-1]) cans[i] = cans[i-1]+1;
+        }
+
+        for (int i=n-2;i>=0;i--){
+            if (ratings[i]>ratings[i+1] && cans[i]<=cans[i+1]) cans[i] = cans[i+1]+1;
+        }
+
+        int res=0;
+        for (int i=0;i<n;i++){
+            System.out.print(cans[i] + " ");
+            res+=cans[i];
+        }
+
+        return res;
+    }
     
 }
